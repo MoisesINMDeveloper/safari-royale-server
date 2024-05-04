c6e7a89475eb5055f6cfb943fbd031ae3e626c62
Moises Navarro <MoisesINMDeveloper@hotmail.com>
2024-05-04 18:18:13 -0400

    Agregar phone a tabla users para obtener mas informacion relevante del usuario

--- dist/server.js
+++ dist/server.js
@@ -5,7 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
 const cors_1 = __importDefault(require("cors"));
-app_1.default.use((0, cors_1.default)());
+app_1.default.use((0, cors_1.default)({
+    origin: "*",
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+}));
 const PORT = process.env.PORT;
 app_1.default.listen(PORT, () => {
     console.log(`Server is running on PORT:${PORT}`);
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import app from "./app";
 import cors from "cors";
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
 
 const PORT = process.env.PORT;
 
