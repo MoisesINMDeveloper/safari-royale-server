48e4788aa969b936815938380497d0793ab05f20
Moises Navarro <MoisesINMDeveloper@hotmail.com>
2024-05-04 12:23:39 -0400

    sigo con las cors

--- dist/server.js
+++ dist/server.js
@@ -3,13 +3,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
+const app_1 = __importDefault(require("./app"));
 const cors_1 = __importDefault(require("cors"));
-const app = (0, express_1.default)();
-// Middleware de CORS
-app.use((0, cors_1.default)());
-// Resto de la configuración de tu aplicación...
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app_1.default.use((0, cors_1.default)({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "*",
+}));
+const PORT = process.env.PORT;
+app_1.default.listen(PORT, () => {
     console.log(`Server is running on PORT:${PORT}`);
 });
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
-import express from "express";
+import app from "./app";
 import cors from "cors";
 
-const app = express();
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "*",
+  })
+);
 
-// Middleware de CORS
-app.use(cors());
-
-// Resto de la configuración de tu aplicación...
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT:${PORT}`);
