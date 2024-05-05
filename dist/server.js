c14c3bfe64d27e9f6822cb22a95fa9c39fdef43f
Moises Navarro <MoisesINMDeveloper@hotmail.com>
2024-05-04 21:28:45 -0400

    Agregar phone a tabla users para obtener mas informacion relevante del usuario

--- dist/server.js
+++ dist/server.js
@@ -3,10 +3,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = __importDefault(require("./app"));
+const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
-const PORT = process.env.PORT;
-app_1.default.use((0, cors_1.default)());
-app_1.default.listen(PORT, () => {
+const app = (0, express_1.default)();
+// Configuración de opciones de CORS
+const corsOptions = {
+    origin: "http://localhost:3000", // Permitir solicitudes desde localhost:3000
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+};
+// Aplicar las opciones de CORS al middleware de Express
+app.use((0, cors_1.default)(corsOptions));
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
     console.log(`Server is running on PORT:${PORT}`);
 });
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,19 @@
-import app from "./app";
+import express from "express";
 import cors from "cors";
 
-const PORT = process.env.PORT;
-app.use(cors());
+const app = express();
+
+// Configuración de opciones de CORS
+const corsOptions = {
+  origin: "http://localhost:3000", // Permitir solicitudes desde localhost:3000
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+};
+
+// Aplicar las opciones de CORS al middleware de Express
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT:${PORT}`);
