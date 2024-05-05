b6469be47498dedcc1d41065ba38e9f2ce1b92ab
Moises Navarro <MoisesINMDeveloper@hotmail.com>
2024-05-04 20:40:04 -0400

    Agregar phone a tabla users para obtener mas informacion relevante del usuario

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,9 @@
-import express from "express";
+import app from "./app";
 import cors from "cors";
 
-const app = express();
+app.use(cors());
 
-const whitelist = [
-  "http://localhost:3000", // Agregar dominios locales
-  "http://127.0.0.1:3000",
-  "https://www.google.com", // Ejemplo de dominio de buscador
-  "https://www.bing.com", // Otro ejemplo de dominio de buscador
-];
-
-const corsOptions = {
-  origin: whitelist,
-  methods: "GET,POST,PUT,DELETE",
-  allowedHeaders: "Content-Type,Authorization",
-};
-
-app.use(cors(corsOptions));
-
-// Resto de la configuración de tu aplicación...
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT:${PORT}`);
