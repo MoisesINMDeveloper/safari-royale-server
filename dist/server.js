1d3512551f38bc8d9beb4f1ed8f98567f99f42a4
Moises Navarro <MoisesINMDeveloper@hotmail.com>
2024-05-04 21:37:33 -0400

    Agregar phone a tabla users para obtener mas informacion relevante del usuario

--- dist/server.js
+++ dist/server.js
@@ -3,18 +3,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
+const app_1 = __importDefault(require("./app"));
 const cors_1 = __importDefault(require("cors"));
-const app = (0, express_1.default)();
-// Configuración de opciones de CORS
-const corsOptions = {
-    origin: "http://localhost:3000", // Permitir solicitudes desde localhost:3000
-    methods: "GET,POST,PUT,DELETE",
-    allowedHeaders: "Content-Type,Authorization",
-};
-// Aplicar las opciones de CORS al middleware de Express
-app.use((0, cors_1.default)(corsOptions));
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
+app_1.default.use((0, cors_1.default)({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "*",
+}));
+const PORT = process.env.PORT;
+app_1.default.listen(PORT, () => {
     console.log(`Server is running on PORT:${PORT}`);
 });
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,15 @@
-import express from "express";
+import app from "./app";
 import cors from "cors";
 
-const app = express();
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "*",
+  })
+);
 
-// Configuración de opciones de CORS
-const corsOptions = {
-  origin: "http://localhost:3000", // Permitir solicitudes desde localhost:3000
-  methods: "GET,POST,PUT,DELETE",
-  allowedHeaders: "Content-Type,Authorization",
-};
-
-// Aplicar las opciones de CORS al middleware de Express
-app.use(cors(corsOptions));
-
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT:${PORT}`);
