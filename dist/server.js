b2f68b12566cf2e58a844968d75635154a25bfea
Moises Navarro <MoisesINMDeveloper@hotmail.com>
2024-05-04 21:03:04 -0400

    Agregar phone a tabla users para obtener mas informacion relevante del usuario

--- dist/server.js
+++ dist/server.js
@@ -6,8 +6,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
 const cors_1 = __importDefault(require("cors"));
 const whiteList = ["http://localhost:3000"];
-app_1.default.use((0, cors_1.default)({ origin: whiteList }));
 const PORT = process.env.PORT;
 app_1.default.listen(PORT, () => {
     console.log(`Server is running on PORT:${PORT}`);
 });
+app_1.default.use((0, cors_1.default)({ origin: whiteList }));
--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,10 @@ import app from "./app";
 import cors from "cors";
 
 const whiteList = ["http://localhost:3000"];
-app.use(cors({ origin: whiteList }));
 
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT:${PORT}`);
 });
+app.use(cors({ origin: whiteList }));
